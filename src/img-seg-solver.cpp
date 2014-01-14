9514b52c2923cd8f268de451ab946c353bb90f56
Heather Michaud <hmm34@zips.uakron.edu>
2014-01-14 17:12:15 -0500

    Add empty main method.
    The class uses the required javadoc, but we should consider
    Doxygen instead, since it's more C++ friendly.

---
+++ src/img-seg-solver.cpp
@@ -0,0 +1,21 @@
+/**
+ * COPYRIGHT (C) 2014 Drew Guarnera & Heather Michaud. All rights reserved.
+ *
+ * @brief Demonstrate the ability to solve the image segmentation problem using 
+ * 	the Ford-Fulkerson algorithm.
+ * @author Drew Guarnera & Heather Michaud
+ * @version 0.9
+ */
+
+/**
+ * Executes the img-seg-solver program.
+ *
+ * @param argc Number of arguments
+ * @param argv List of arguments
+ * @return Error or success code
+*/
+int main(int argc, char* argv[])
+{
+
+	return 0;
+}
