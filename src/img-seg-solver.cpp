c76f2ba6b9623bee5eb47b0256a5902fba5d5233
Heather Michaud <hmm34@zips.uakron.edu>
2014-01-17 14:40:12 -0500

    Change javadoc to doxygen

--- src/img-seg-solver.cpp
+++ src/img-seg-solver.cpp
@@ -1,21 +1,21 @@
-/**
- * COPYRIGHT (C) 2014 Drew Guarnera & Heather Michaud. All rights reserved.
- *
- * @brief Demonstrate the ability to solve the image segmentation problem using 
- * 	the Ford-Fulkerson algorithm.
- * @author Drew Guarnera & Heather Michaud
- * @version 0.9
- */
-
-/**
- * Executes the img-seg-solver program.
- *
- * @param argc Number of arguments
- * @param argv List of arguments
- * @return Error or success code
+/*
+	@brief Demonstrates the ability to solve the image segmentation problem
+		using the Ford-Fulkerson algorithm.
+	@author Drew Guarnera & Heather Michaud
+	@version 0.9
+	@copyright Copyright 2014 Guarnera & Michaud. All rights reserved.
 */
+
+#include <stdio>	
+
+//! @brief Entry point to execute the img-seg-solver program
+//! @param argc Number of arguments
+//! @param argv List of arguments
+//! @return Error or success code
 int main(int argc, char* argv[])
 {
+	// Store the graph in adjacency-list representation
+	std::vector<std::list<vertex>> graph;
 
 	return 0;
 }
