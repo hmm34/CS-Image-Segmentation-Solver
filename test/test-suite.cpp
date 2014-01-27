421cb51b1a9eb60579be68b5d1f779920ec18e40
Heather Michaud <hmm34@zips.uakron.edu>
2014-01-27 17:37:19 -0500

    Add rapid pseudo-random number generator

--- test/test-suite.cpp
+++ test/test-suite.cpp
@@ -8,6 +8,24 @@
 
 #include <assert.h>
 
+//! @brief Q&D implementation of Marsaglia's xorshift random number generator. Rand() is way too slow to obtain
+//!	 random numbers rapidly and in repetition. See http://en.wikipedia.org/wiki/Xorshift
+//! @retval Pseudo-random number that passes the diehard tests.
+uint32_t xorshift()
+{
+	static uint32_t x = 123456789;
+  	static uint32_t y = 362436069;
+  	static uint32_t z = 521288629;
+  	static uint32_t w = 88675123;
+  	uint32_t t;
+ 
+  	t = x ^ (x << 11);
+  	x = y; y = z; z = w;
+  	return w = w ^ (w >> 19) ^ (t ^ (t >> 8));
+}
+
+//! @brief Entry point to test suite execution
+//! @retval Error or success code
 int main()
 {
 
