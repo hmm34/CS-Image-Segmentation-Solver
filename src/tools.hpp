51136d6cfb54ffb45c82a53c1873218d6207ac2a
Heather Michaud <hmm34@zips.uakron.edu>
2014-02-01 19:30:03 -0500

    Clean up doxygen

--- src/tools.hpp
+++ src/tools.hpp
@@ -23,17 +23,8 @@ namespace tools {
 	 	their edge weights. Line 2 shows the vertices that vertex 1 is conected to, with their weights - and so on. The 
 	 	first line shows that vertex 0 is connected to vertex 1 with edge weight 3, vertex 2 with weight 6, and vertex 3 
 	 	with edge weight 8.
-	 	
-	 	@note This allocates memory for the returned graph. The user must free memory after use.
 	*/
 	 void graphFromFile(const char* file, graph& g);
-
-	//! @brief Ford fulkerson algorithm used to obtain the maximum flow and minimum cut
-	//! @param g The graph on which to perform the algorithm
-	//! @param source 
-	//! @param sink
-	//! @retval The maximum flow for the given graph
-	int fordFulkerson(graph& g, int source, int sink);
 
 	//! @brief Performs a breadth first search on the graph to obtain the shortest path and minimum capacity in the path
 	//! @param g The graph to perform the breadth first search on
@@ -44,4 +35,11 @@ namespace tools {
 	//! @retval An empty vector if the start or end nodes were invalid, or the end node could not be found, with a 
 	//!  negative minimum capacity
 	std::pair< std::vector<int>, int> breadthFirstSearch(graph& g, int start, int end);
+
+	//! @brief Ford fulkerson algorithm used to obtain the maximum flow and minimum cut
+	//! @param g The graph on which to perform the algorithm
+	//! @param source 
+	//! @param sink
+	//! @retval The maximum flow for the given graph
+	int fordFulkerson(graph& g, int source, int sink);
 }
