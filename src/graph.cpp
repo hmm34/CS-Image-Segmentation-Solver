b83e46614f70856e69aae4ce90b63cbe3b6b73fe
Drew Guarnera <dtg3@zips.uakron.edu>
2014-02-03 11:43:25 -0500

    Brackets for graph class coding standards...

--- src/graph.cpp
+++ src/graph.cpp
@@ -9,17 +9,17 @@
 #include <sstream>
 #include <algorithm>
 
-graph::graph() { 
-}
+graph::graph() {}
 
-graph::~graph() {
-}
+graph::~graph() {}
 
-bool graph::addNode(int id) {
+bool graph::addNode(int id)
+{
 	sNodes.insert(id);
 
 	// Only add if this node ID doesn't already exists in the list
-	if (adjList.find(id) == adjList.end()) {
+	if (adjList.find(id) == adjList.end())
+	{
 		std::map<int, vertex> emptyNeighbors;
 		adjList[id] = emptyNeighbors;
 		return true;
@@ -27,32 +27,38 @@ bool graph::addNode(int id) {
 	return false;
 }
 
-bool graph::addNeighbor(int fromID, vertex neighborNode) {
+bool graph::addNeighbor(int fromID, vertex neighborNode)
+{
 	sNodes.insert(neighborNode.id);
 
 	// Only add if this neighboring node ID doesn't already exists in the list
-	if (adjList[fromID].find(neighborNode.id) == adjList[fromID].end()) {
+	if (adjList[fromID].find(neighborNode.id) == adjList[fromID].end())
+	{
 		adjList[fromID][neighborNode.id] = neighborNode;
 		return true;
 	}
 	return false;
 }
 
-int graph::nodes() {
+int graph::nodes()
+{
 	return sNodes.size();
 }
 
-void graph::print() {
+void graph::print()
+{
 	std::map<int, std::map<int, vertex> >::iterator adjItr = adjList.begin();
 	std::map<int, std::map<int, vertex> >::iterator adjEnd = adjList.end();
-	while (adjItr != adjEnd) {
+	while (adjItr != adjEnd)
+	{
 		// Current vertex
 		std::cerr << (*adjItr).first;
 		std::map<int, vertex>::iterator neighborsItr = (*adjItr).second.begin();
 		std::map<int, vertex>::iterator neighborsEnd = (*adjItr).second.end();
 
 		// Neighboring vertices
-		while (neighborsItr != neighborsEnd) {	
+		while (neighborsItr != neighborsEnd)
+		{	
 			std::cerr << " --(" << (*neighborsItr).second.weight << ")--> " << (*neighborsItr).second.id;
 			++neighborsItr;
 		}
--- src/graph.hpp
+++ src/graph.hpp
@@ -11,40 +11,41 @@
 #include <map>
 
 //! @brief The graph is a list of connected vertices
-struct vertex {
+struct vertex
+{
 	int id;		//! Unique ID representing which vertex is held
 	int weight; //! The capacity or weight of this connected node
 };
 
 //! @brief Graph object to contain vertices and weighted edges in adjacency list format.
-class graph {
+class graph
+{
 
-public:
-	//! @brief Basic constructor
-	graph();
+	public:
+		//! @brief Basic constructor
+		graph();
 
-	//! @brief Basic destructor
-	~graph();
+		//! @brief Basic destructor
+		~graph();
 
-	//! @brief Adds a vertex (node) to the adjacency list
-	//! @param id The ID of the vertex (node)
-	//! @retval true if successful, false if the node already exists
-	bool addNode(int id);
+		//! @brief Adds a vertex (node) to the adjacency list
+		//! @param id The ID of the vertex (node)
+		//! @retval true if successful, false if the node already exists
+		bool addNode(int id);
 
-	//! @brief Adds a neighboring vertex (node) to the given vertex in the adjacency list
-	//! @brief fromID The ID of the vertex whose neighbor will be added
-	//! @param neighborNode The neighboring node with an ID and weight of the connected edge
-	//! @retval true if successful, false if the node already exists
-	bool addNeighbor(int fromID, vertex neighborNode);
+		//! @brief Adds a neighboring vertex (node) to the given vertex in the adjacency list
+		//! @brief fromID The ID of the vertex whose neighbor will be added
+		//! @param neighborNode The neighboring node with an ID and weight of the connected edge
+		//! @retval true if successful, false if the node already exists
+		bool addNeighbor(int fromID, vertex neighborNode);
 
-	//! @brief Get the number of nodes contained within this graph
-	//! @retval The number of unique nodes in the graph
-	int nodes();
+		//! @brief Get the number of nodes contained within this graph
+		//! @retval The number of unique nodes in the graph
+		int nodes();
 
-	//! @brief Prints the graph out in adjacency list format
-	void print();
+		//! @brief Prints the graph out in adjacency list format
+		void print();
 
-	std::map<int, std::map<int, vertex> > adjList;	//!< Adjacency list representation of the directed graph
-	std::set<int> sNodes;							//!< Unique set of all nodes
-
+		std::map<int, std::map<int, vertex> > adjList;	//!< Adjacency list representation of the directed graph
+		std::set<int> sNodes;							//!< Unique set of all nodes
 };
