1719a0951b417a80b337b4070a4af876f869f819
Bogadamidi <bharath.vitu@gmail.com>
2014-02-07 20:06:23 -0500

    Tools.hpp file changes

--- src/tools.hpp
+++ src/tools.hpp
@@ -7,12 +7,12 @@
 
 #pragma once
 
-#include "graph.hpp"
-#include "pgm.hpp"
+#include "Graph.hpp"
+#include "Pgm.hpp"
 #include <set>
 #include <vector>
 
-namespace tools 
+namespace Tools 
 {
 	//! @brief Reads from a text file
 	//! @param file The name of the file to be read
@@ -27,7 +27,7 @@ namespace tools
 	 	first line shows that vertex 0 is connected to vertex 1 with edge weight 3, vertex 2 with weight 6, and vertex 3 
 	 	with edge weight 8.
 	*/
-	void graphFromFile(const char* file, graph& g);
+	void graphFromFile(const char* file, Graph& g);
 
 	//! @brief Performs a breadth first search on the graph to obtain the shortest path and minimum capacity in the path
 	//! @param g The graph to perform the breadth first search on
@@ -37,14 +37,14 @@ namespace tools
 	//!	 starting node to ending node, and the minimum capacity (weight) along that path
 	//! @retval An empty vector if the start or end nodes were invalid, or the end node could not be found, with a 
 	//!  negative minimum capacity
-	std::pair< std::vector<int>, int> breadthFirstSearch(graph& g, int start, int end);
+	std::pair< std::vector<int>, int> breadthFirstSearch(Graph& g, int start, int end);
 
 	//! @brief Ford fulkerson algorithm used to obtain the maximum flow and minimum cut
 	//! @param g The graph on which to perform the algorithm
 	//! @param source 
 	//! @param sink
 	//! @retval The maximum flow for the given graph
-	int fordFulkerson(graph& g, int source, int sink);
+	int fordFulkerson(Graph& g, int source, int sink);
 
 	//! @brief Solves the image segmentation problem using ford fulkerson, separating the foreground from the background
 	//! @param file The pgm image to be segmented
